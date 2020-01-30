4d50997e0bf904574d00cb9fdad889c36d4f8b71
Ektaynot <40837206+Ektaynot@users.noreply.github.com>
2020-01-30 18:26:33 +0300

    Update autoinstaller.py

--- autoinstaller.py
+++ autoinstaller.py
@@ -1,7 +1,7 @@
 import os
 def cod(ad):
     os.system("sudo apt update")
-    os.system(("sudo apt -y install"+ad))
+    os.system(("sudo apt -y install"+" "+ad))
 sayı="a"
 while sayı != "q":
     sayı=input("lütfen indirmek istediğiniz şeyi yazın(durmak için q yazın):")
@@ -13,6 +13,7 @@ while sayı != "q":
 oku=open("willdownload.txt","r")
 select=input("Aşağıdaki ürünler indirilicek kabul ediyorsanız evet yazın"+"\n"+oku.readline())
 if select == "evet":
+    oku=open("willdownload.txt","r")
     cod((oku.readline()))
 else:
     raise SystemExit
