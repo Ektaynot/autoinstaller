968c3484620681237a56c91507a7660a8042e473
Ektaynot <40837206+Ektaynot@users.noreply.github.com>
2020-01-30 18:01:29 +0300

    Create autoinstaller.py

---
+++ autoinstaller.py
@@ -0,0 +1,18 @@
+import os
+def cod(ad):
+    os.system("sudo apt update")
+    os.system(("sudo apt -y install"+ad))
+sayı="a"
+while sayı != "q":
+    sayı=input("lütfen indirmek istediğiniz şeyi yazın(durmak için q yazın):")
+    dosya = open("willdownload.txt","a")
+    if sayı == "q":
+        break
+    else:
+        dosya.write(sayı+" ")
+oku=open("willdownload.txt","r")
+select=input("Aşağıdaki ürünler indirilicek kabul ediyorsanız evet yazın"+"\n"+oku.readline())
+if select == "evet":
+    cod((oku.readline()))
+else:
+    raise SystemExit
