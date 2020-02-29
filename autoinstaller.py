57aef080841b69094a258c632141967812cb0f13
Ektaynot <40837206+Ektaynot@users.noreply.github.com>
2020-02-29 21:33:06 +0300

    Update autoinstaller.py

--- autoinstaller.py
+++ autoinstaller.py
@@ -1,18 +1,26 @@
 import os
+system=input("Select Your Package Manager:\n1:APT\n2:Pacman\n3:Zypper")
 def cod(ad):
-    os.system("sudo apt update")
-    os.system(("sudo apt -y install"+" "+ad))
+    if system == "1":
+        os.system("sudo apt update")
+        os.system(("sudo apt -y install "+ad))
+    elif system == "2":
+        os.system("sudo pacman -Syy")
+        os.system(("sudo pacman -Sy --noconfirm "+ad))
+    elif system == "3":
+        os.system("sudo zypper update")
+        os.system(("sudo zypper -y install "+ad))
 sayı="a"
 while sayı != "q":
-    sayı=input("lütfen indirmek istediğiniz şeyi yazın(durmak için q yazın):")
+    sayı=input("Please write the things you want to install (write q to exit):")
     dosya = open("willdownload.txt","a")
     if sayı == "q":
         break
     else:
         dosya.write(sayı+" ")
 oku=open("willdownload.txt","r")
-select=input("Aşağıdaki ürünler indirilicek kabul ediyorsanız evet yazın"+"\n"+oku.readline())
-if select == "evet":
+select=input("If you are okay with the things down below write yes"+"\n"+oku.readline())
+if select == "evet" or "yes":
     oku=open("willdownload.txt","r")
     cod((oku.readline()))
 else:
